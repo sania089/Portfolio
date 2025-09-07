5b10e2440735452a37bc72ef47fa25446453da48
pranay-anekantvad <pranay@anekantvad.com>
2025-09-07 17:25:49 +0530

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/mainResume (2).pdf";
+import pdf from "../../Assets/../Assets/Sania_Sarin_Resume.pdf";
 import { AiOutlineDownload, AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
